079eaf12baa1ce908b841a5e685d5442ead520c8
Jason Humber <jlhumber@openiii.com>
2025-12-14 16:29:16 -0600

    #2 add `types.py` with `QueryComponents` and `OverlayConfig` definitions

---
+++ geomio/shared/types.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Types
+"""
+
+
+from typing import Any, NamedTuple
+
+from shapely.geometry.multipolygon import MultiPolygon as ShapelyMultiPolygon
+
+
+class QueryComponents(NamedTuple):
+    """
+    Spatial Query Components
+    """
+    use_index: bool
+    has_intersection: bool
+    sql_touches: str
+    sql_outside: str
+    sql_insert: str
+# End QueryComponents class
+
+
+class OverlayConfig(NamedTuple):
+    """
+    Overlay Configuration
+    """
+    fudgeo_cls: Any
+    is_multi: bool
+    shapely_multi_cls: Any
+    shapely_types: tuple
+    geometry: ShapelyMultiPolygon
+# End OverlayConfig class
+
+
+if __name__ == '__main__':  # pragma: no cover
+    pass
