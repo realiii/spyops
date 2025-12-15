45ffa7695e4ccf78e749a1bdc0d226b099fb9556
Jason Humber <jlhumber@openiii.com>
2025-12-14 19:19:51 -0600

    #2 add clip function and associated tests

--- geomio/analysis/extract.py
+++ geomio/analysis/extract.py
@@ -7,21 +7,27 @@ from typing import Callable
 
 from fudgeo import FeatureClass, Table
 from fudgeo.constant import FETCH_SIZE
+from shapely.io import from_wkb
 
-from geomio.analysis.sql import build_sql_select_by_attributes
+from geomio.analysis.sql import (
+    build_query_components, build_sql_select_by_attributes)
 from geomio.analysis.util import shared_select
 from geomio.shared.constants import (
-    GEOPACKAGE, GROUP_FIELDS, SOURCE, SQL_EMPTY, TARGET)
-from geomio.shared.field import TEXT_AND_NUMBERS, make_field_names
-from geomio.shared.hints import ELEMENT, FIELDS, FIELD_NAMES, GPKG
-from geomio.shared.util import element_names, make_unique_name
+    GEOPACKAGE, GROUP_FIELDS, OPERATOR, SOURCE, SQL_EMPTY, TARGET)
+from geomio.shared.field import (
+    GEOM_TYPE_POLYGONS, TEXT_AND_NUMBERS, make_field_names)
+from geomio.shared.geometry import overlay_config
+from geomio.shared.hints import ELEMENT, FIELDS, FIELD_NAMES, FLOAT, GPKG
+from geomio.shared.util import (
+    add_spatial_index, element_names, extend_records, make_unique_name)
 from geomio.shared.validation import (
     validate_element, validate_feature_class, validate_field,
-    validate_geopackage, validate_result, validate_table)
+    validate_geopackage, validate_result, validate_same_crs, validate_table,
+    validate_xy_tolerance)
 
 
 __all__ = ['table_select', 'select', 'extract_rows', 'extract_features',
-           'split_by_attributes']
+           'split_by_attributes', 'clip']
 
 
 @validate_result()
@@ -90,6 +96,48 @@ def split_by_attributes(source: ELEMENT, group_fields: FIELDS | FIELD_NAMES,
                     insert_sql.format(element.escaped_name), records)
     return elements
 # End split_by_attributes function
+
+
+@validate_result()
+@validate_same_crs(SOURCE, OPERATOR)
+@validate_xy_tolerance()
+@validate_feature_class(TARGET, exists=False)
+@validate_feature_class(OPERATOR, geometry_types=GEOM_TYPE_POLYGONS)
+@validate_feature_class(SOURCE)
+def clip(source: FeatureClass, operator: FeatureClass, target: FeatureClass,
+         overwrite: bool = False, xy_tolerance: FLOAT = None) -> FeatureClass:
+    """
+    Clip
+
+    Extracts features using the features of a polygon feature class. Extracted
+    features are cut along the edges of the clipping polygons.
+    """
+    components = build_query_components(
+        source, target=target, operator=operator)
+    target = shared_select(
+        source=source, target=target, where_clause=SQL_EMPTY,
+        overwrite=overwrite)
+    if not components.has_intersection:
+        return target
+    records = []
+    config = overlay_config(source, operator=operator)
+    polygon = config.geometry
+    with target.geopackage.connection as conn:
+        cursor = source.geopackage.connection.execute(components.sql_touches)
+        while features := cursor.fetchmany(FETCH_SIZE):
+            geometries = [from_wkb(g.wkb) for g, *_ in features]
+            intersects = polygon.intersects(geometries)
+            if not intersects.any():
+                continue
+            keepers = [f for f, keep in zip(features, intersects) if keep]
+            geometries = [g for g, keep in zip(geometries, intersects) if keep]
+            results = [(g, polygon.intersection(geom, grid_size=xy_tolerance), attrs)
+                       for geom, (g, *attrs) in zip(geometries, keepers)]
+            extend_records(results, records=records, config=config)
+            conn.executemany(components.sql_insert, records)
+            records.clear()
+    return target
+# End clip function
 
 
 # Aliases
