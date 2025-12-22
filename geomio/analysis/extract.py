c3b066dfdb18749b08027bd19dd21137de11f944
Jason Humber <jlhumber@openiii.com>
2025-12-22 17:45:01 -0600

    #10 Refactor: Replace `build_query_components` with `build_analysis` in `extract` logic and update related SQL call

--- geomio/analysis/extract.py
+++ geomio/analysis/extract.py
@@ -12,10 +12,10 @@ from shapely import box
 from shapely.io import from_wkb
 
 from geomio.analysis.sql import (
-    build_query_components, build_sql_select_by_attributes)
+    build_analysis, build_sql_select_by_attributes)
 from geomio.shared.constant import (
     FIELD, GROUP_FIELDS, OPERATOR, SOURCE, SQL_EMPTY, TARGET, UNDERSCORE)
-from geomio.shared.element import copy_element, copy_feature_class
+from geomio.shared.element import copy_element
 from geomio.shared.field import (
     GEOM_TYPE_POLYGONS, TEXTS, TEXT_AND_NUMBERS, make_field_names)
 from geomio.shared.geometry import extent_from_feature_class, overlay_config
@@ -112,17 +112,15 @@ def clip(source: FeatureClass, operator: FeatureClass, target: FeatureClass, *,
     Extracts features using the features of a polygon feature class. Extracted
     features are cut along the edges of the clipping polygons.
     """
-    components = build_query_components(
-        source, target=target, operator=operator)
-    target = copy_feature_class(
-        source=source, target=target, where_clause=SQL_EMPTY)
+    components = build_analysis(
+        source, target=target, operator=operator, use_empty=True)
     if not components.has_intersection:
-        return target
+        return components.target
     records = []
     config = overlay_config(source, operator=operator)
     polygon = config.geometry
     with target.geopackage.connection as conn:
-        cursor = source.geopackage.connection.execute(components.sql_touches)
+        cursor = source.geopackage.connection.execute(components.sql_intersect)
         while features := cursor.fetchmany(FETCH_SIZE):
             geometries = [from_wkb(g.wkb) for g, *_ in features]
             intersects = polygon.intersects(geometries)
