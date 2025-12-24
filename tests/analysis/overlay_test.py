4a6909a1e473263f0dc3b33be7ce94303b20af62
Jason Humber <jlhumber@openiii.com>
2025-12-24 08:55:02 -0600

    #12 Refactor: Split and optimize `test_erase` in `overlay_test.py` for clarity and performance. Update `build_analysis` import from `util`.

--- tests/analysis/overlay_test.py
+++ tests/analysis/overlay_test.py
@@ -8,7 +8,7 @@ from fudgeo import FeatureClass
 from pytest import mark, param
 
 from geomio.analysis.overlay import erase
-from geomio.analysis.sql import build_analysis
+from geomio.analysis.util import build_analysis
 
 pytestmark = [mark.overlay]
 
@@ -33,21 +33,41 @@ pytestmark = [mark.overlay]
     ('airports_mp_p', 1, 10),
     ('roads_mp_l', 1, 14),
 ])
-def test_erase(inputs, world_features, fresh_gpkg, fc_name, xy_tolerance, count):
+def test_erase_reduced(inputs, world_features, fresh_gpkg, fc_name, xy_tolerance, count):
     """
-    Test erase
+    Test erase -- reduced data for faster testing
     """
     eraser = inputs['eraser_a']
     assert eraser.count == 5
     source = world_features[fc_name]
     assert source.is_multi_part == ('mp' in fc_name)
     target = FeatureClass(geopackage=fresh_gpkg, name=f'temp_{fc_name}')
-    components = build_analysis(source=source, target=target, operator=eraser, use_empty=True)
-    _, touches = components.sql_intersect.split('WHERE', 1)
+    ac = build_analysis(source=source, target=target, operator=eraser)
+    _, touches = ac.query.select.split('WHERE', 1)
     subset = source.copy(f'subset_{fc_name}', where_clause=touches, geopackage=fresh_gpkg)
     assert subset.count <= source.count
     target = FeatureClass(geopackage=fresh_gpkg, name=fc_name)
     result = erase(source=subset, operator=eraser, target=target, xy_tolerance=xy_tolerance)
+    assert result.count == count
+# End test_erase_reduced function
+
+
+@mark.parametrize('fc_name, xy_tolerance, count', [
+    ('airports_p', None, 3464),
+    ('airports_mp_p', None, 191),
+    ('airports_p', 0.001, 3464),
+    ('airports_mp_p', 0.001, 191),
+])
+def test_erase(inputs, world_features, fresh_gpkg, fc_name, xy_tolerance, count):
+    """
+    Test erase - ensure that disjoint is exercised
+    """
+    eraser = inputs['eraser_a']
+    assert eraser.count == 5
+    source = world_features[fc_name]
+    assert source.is_multi_part == ('mp' in fc_name)
+    target = FeatureClass(geopackage=fresh_gpkg, name=fc_name)
+    result = erase(source=source, operator=eraser, target=target, xy_tolerance=xy_tolerance)
     assert result.count == count
 # End test_erase function
 
