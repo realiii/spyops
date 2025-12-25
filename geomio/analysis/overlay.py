8d47df124ccec47703dd2851360c0db9583232b1
Jason Humber <jlhumber@openiii.com>
2025-12-25 09:17:09 -0600

    #12 Add `intersect` function to `overlay.py` and corresponding tests in `overlay_test.py` for intersection analysis

--- geomio/analysis/overlay.py
+++ geomio/analysis/overlay.py
@@ -4,17 +4,17 @@ Overlay
 """
 
 
+from collections import defaultdict
+
 from fudgeo import FeatureClass
 from fudgeo.constant import FETCH_SIZE
-from shapely import set_precision
+from shapely import STRtree, set_precision
 from shapely.io import from_wkb
 
-from geomio.shared.base import OverlayConfig
 from geomio.shared.constant import OPERATOR, SOURCE, TARGET
 from geomio.shared.field import GEOM_TYPE_POLYGONS
-from geomio.shared.geometry import overlay_config
 from geomio.shared.hint import XY_TOL
-from geomio.shared.query import QueryErase
+from geomio.shared.query import QueryErase, QueryIntersect
 from geomio.shared.util import extend_records
 from geomio.shared.validation import (
     validate_feature_class, validate_result, validate_same_crs,
@@ -65,6 +65,61 @@ def erase(source: FeatureClass, operator: FeatureClass, target: FeatureClass, *,
 # End erase function
 
 
+@validate_result()
+@validate_same_crs(SOURCE, OPERATOR)
+@validate_xy_tolerance()
+@validate_feature_class(TARGET, exists=False)
+@validate_feature_class(OPERATOR, geometry_types=GEOM_TYPE_POLYGONS)
+@validate_feature_class(SOURCE)
+def intersect(source: FeatureClass, operator: FeatureClass,
+              target: FeatureClass, *, xy_tolerance: XY_TOL = None) -> FeatureClass:
+    """
+    Intersect
+
+    Extracts the portion of the input feature class that overlaps with the
+    operator feature class.  Optionally, extends the output feature class
+    with attributes from the operator feature class.
+    """
+    query = QueryIntersect(source, target=target, operator=operator)
+    if not query.has_intersection:
+        return query.target_empty
+    with query.operator.geopackage.connection as cin:
+        cursor = cin.execute(query.select_operator)
+        op_features = cursor.fetchall()
+        op_geoms = [from_wkb(g.wkb) for g, *_ in op_features]
+    records = []
+    tree = STRtree(op_geoms)
+    with (query.target.geopackage.connection as cout,
+          query.source.geopackage.connection as cin):
+        cursor = cin.execute(query.select)
+        while features := cursor.fetchmany(FETCH_SIZE):
+            geometries = [from_wkb(g.wkb) for g, *_ in features]
+            intersects = tree.query(geometries, predicate='intersects')
+            if not len(intersects):
+                continue
+            grouper = defaultdict(list)
+            for src_idx, op_idx in intersects.T.tolist():
+                grouper[op_idx].append(src_idx)
+            results = []
+            for op_idx, indexes in grouper.items():
+                op_attr = op_features[op_idx][1:]
+                op_geom = op_geoms[op_idx]
+                src_attrs = [features[idx][1:] for idx in indexes]
+                if xy_tolerance is not None:
+                    intersections = op_geom.intersection(
+                        [geometries[idx] for idx in indexes],
+                        grid_size=xy_tolerance)
+                else:
+                    intersections = op_geom.intersection(
+                        [geometries[idx] for idx in indexes])
+                results.extend([
+                    (g, src_attr)
+                    for g, src_attr in zip(intersections, src_attrs)])
+            extend_records(results, records=records, config=query.config)
+            cout.executemany(query.insert, records)
+            records.clear()
+    return query.target
+# End intersect function
 
 
 def _process_disjoint(query: QueryErase, xy_tolerance: XY_TOL) -> None:
--- tests/analysis/overlay_test.py
+++ tests/analysis/overlay_test.py
@@ -7,8 +7,11 @@ Tests for Overlay
 from fudgeo import FeatureClass
 from pytest import mark, param
 
-from geomio.analysis.overlay import erase
+from geomio.analysis.overlay import erase, intersect
+from geomio.shared.enumeration import Setting
 from geomio.shared.query import QueryErase
+from geomio.shared.setting import Swap
+
 
 pytestmark = [mark.overlay]
 
@@ -70,6 +73,41 @@ def test_erase(inputs, world_features, fresh_gpkg, fc_name, xy_tolerance, count)
     result = erase(source=source, operator=eraser, target=target, xy_tolerance=xy_tolerance)
     assert result.count == count
 # End test_erase function
+
+
+@mark.parametrize('fc_name, xy_tolerance, count', [
+    ('admin_a', None, 114),
+    ('airports_p', None, 40),
+    ('roads_l', None, 2514),
+    ('admin_mp_a', None, 68),
+    ('airports_mp_p', None, 8),
+    ('roads_mp_l', None, 14),
+    ('admin_a', 0.001, 112),
+    ('airports_p', 0.001, 40),
+    ('roads_l', 0.001, 3380),
+    ('admin_mp_a', 0.001, 68),
+    ('airports_mp_p', 0.001, 8),
+    ('roads_mp_l', 0.001, 14),
+    ('admin_a', 1, 22),
+    ('airports_p', 1, 35),
+    ('roads_l', 1, 378),
+    ('admin_mp_a', 1, 22),
+    ('airports_mp_p', 1, 8),
+    ('roads_mp_l', 1, 13),
+])
+def test_intersect_setting(inputs, world_features, mem_gpkg, fc_name, xy_tolerance, count):
+    """
+    Test intersect using analysis settings
+    """
+    operator = inputs['intersect_a']
+    assert operator.count == 5
+    source = world_features[fc_name]
+    target = FeatureClass(geopackage=mem_gpkg, name=fc_name)
+    with Swap(Setting.XY_TOLERANCE, xy_tolerance):
+        result = intersect(source=source, operator=operator, target=target)
+    assert result.count < source.count
+    assert result.count == count
+# End test_intersect_setting function
 
 
 if __name__ == '__main__':  # pragma: no cover
