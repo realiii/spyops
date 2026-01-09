f33d67472ece543fb855821c9e5d8e5fb01b8d47
Jason Humber <jlhumber@openiii.com>
2026-01-09 11:11:54 -0600

    #45 #53 #54 Refactor: extract geometry constants to a new `constant` module and update imports in `config` module

--- gisworks/geometry/config.py
+++ gisworks/geometry/config.py
@@ -7,62 +7,13 @@ Geometry Configuration
 from typing import Any, Callable, NamedTuple, Type
 
 from fudgeo import FeatureClass
-from fudgeo.enumeration import GeometryType
-from fudgeo.geometry import (
-    LineString, LineStringM, LineStringZ, LineStringZM, MultiLineString,
-    MultiLineStringM, MultiLineStringZ, MultiLineStringZM, MultiPoint,
-    MultiPointM, MultiPointZ, MultiPointZM, MultiPolygon, MultiPolygonM,
-    MultiPolygonZ, MultiPolygonZM, Point, PointM, PointZ, PointZM, Polygon,
-    PolygonM, PolygonZ, PolygonZM)
 from shapely import (
-    LineString as ShapelyLineString,
-    MultiLineString as ShapelyMultiLineString, MultiPoint as ShapelyMultiPoint,
-    MultiPolygon as ShapelyMultiPolygon, Point as ShapelyPoint,
-    Polygon as ShapelyPolygon, line_merge)
+    LineString, MultiLineString, line_merge)
 from shapely.geometry.base import BaseGeometry, BaseMultipartGeometry
 
+from gisworks.geometry.constant import (
+    FUDGEO_GEOMETRY_LOOKUP, SHAPELY_GEOMETRY_LOOKUP)
 from gisworks.geometry.util import nada
-
-SHAPELY_GEOMETRY_LOOKUP: dict[str, tuple[Any, Any]] = {
-    GeometryType.point: (ShapelyPoint, ShapelyMultiPoint),
-    GeometryType.multi_point: (ShapelyPoint, ShapelyMultiPoint),
-    GeometryType.linestring: (ShapelyLineString, ShapelyMultiLineString),
-    GeometryType.multi_linestring: (ShapelyLineString, ShapelyMultiLineString),
-    GeometryType.polygon: (ShapelyPolygon, ShapelyMultiPolygon),
-    GeometryType.multi_polygon: (ShapelyPolygon, ShapelyMultiPolygon),
-}
-FUDGEO_GEOMETRY_LOOKUP: dict[str, dict[tuple[bool, bool], Any]] = {
-    GeometryType.point: {
-        (False, False): Point,
-        (True, False): PointZ,
-        (False, True): PointM,
-        (True, True): PointZM},
-    GeometryType.multi_point: {
-        (False, False): MultiPoint,
-        (True, False): MultiPointZ,
-        (False, True): MultiPointM,
-        (True, True): MultiPointZM},
-    GeometryType.linestring: {
-        (False, False): LineString,
-        (True, False): LineStringZ,
-        (False, True): LineStringM,
-        (True, True): LineStringZM},
-    GeometryType.multi_linestring: {
-        (False, False): MultiLineString,
-        (True, False): MultiLineStringZ,
-        (False, True): MultiLineStringM,
-        (True, True): MultiLineStringZM},
-    GeometryType.polygon: {
-        (False, False): Polygon,
-        (True, False): PolygonZ,
-        (False, True): PolygonM,
-        (True, True): PolygonZM},
-    GeometryType.multi_polygon: {
-        (False, False): MultiPolygon,
-        (True, False): MultiPolygonZ,
-        (False, True): MultiPolygonM,
-        (True, True): MultiPolygonZM}
-}
 
 
 class GeometryConfig(NamedTuple):
@@ -92,12 +43,12 @@ def geometry_config(target: FeatureClass) -> GeometryConfig:
 # End geometry_config function
 
 
-def _combine_lines(value: ShapelyLineString | ShapelyMultiLineString) \
-        -> ShapelyLineString | ShapelyMultiLineString:
+def _combine_lines(value: LineString | MultiLineString) \
+        -> LineString | MultiLineString:
     """
     Combine Lines using Directed Line Merge
     """
-    if isinstance(value, ShapelyMultiLineString):
+    if isinstance(value, MultiLineString):
         return line_merge(value, directed=True)
     return value
 # End _combine_lines function
@@ -107,7 +58,7 @@ def _get_combiner(filter_types: tuple) -> Callable:
     """
     Get Combiner Function
     """
-    if ShapelyLineString in filter_types:
+    if LineString in filter_types:
         return _combine_lines
     return nada
 # End _get_combiner function
