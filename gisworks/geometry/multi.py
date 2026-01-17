9da0f65a36c8661b81505a482f292e8cc3b5816e
Jason Humber <jlhumber@openiii.com>
2026-01-17 08:33:42 -0600

    #15 Refactor: unify geometry handling functions and update `build_multi` to support multiple input types

--- gisworks/geometry/multi.py
+++ gisworks/geometry/multi.py
@@ -3,102 +3,93 @@
 Build Multi Geometry
 """
 
-
-from typing import Callable, Optional, TYPE_CHECKING
-
-from fudgeo.constant import FETCH_SIZE
+from fudgeo import FeatureClass
 from fudgeo.enumeration import GeometryType
-from shapely import MultiLineString, MultiPoint, MultiPolygon
+from numpy import ndarray
+from shapely import MultiLineString, MultiPoint, MultiPolygon, force_2d
 from shapely.constructive import normalize
 from shapely.creation import prepare
 from shapely.ops import unary_union
 
-from gisworks.geometry.enumeration import DimensionOption
-from gisworks.geometry.util import get_geoms_iter, to_shapely
 from gisworks.geometry.validate import (
-    check_linestring, check_point, check_polygon)
+    _check_geometries, _get_validated_geoms, check_linestring, check_point,
+    check_polygon)
 
 
-if TYPE_CHECKING:  # pragma: no cover
-    from fudgeo import FeatureClass
-    from shapely import MultiLineString, MultiPoint, MultiPolygon
-
-
-def build_multi(feature_class: Optional['FeatureClass']) \
+def build_multi(features: FeatureClass | ndarray | list | None) \
         -> MultiPoint | MultiLineString | MultiPolygon | None:
     """
-    Build MultiPoint, MultiLine or MultiPolygon
+    Build MultiPoint, MultiLineString or MultiPolygon from a Feature Class or
+    collection of geometries.
     """
-    if not feature_class:
+    if not len(features):
         return None
-    shape_type = feature_class.shape_type
+    if isinstance(features, FeatureClass):
+        shape_type = features.shape_type
+    else:
+        shape_type = features[0].geom_type.upper()
+        features = force_2d(features)
     if shape_type in (GeometryType.point, GeometryType.multi_point):
-        return _build_multi_point(feature_class)
+        return _multi_point(features)
     elif shape_type in (GeometryType.linestring, GeometryType.multi_linestring):
-        return _build_multi_linestring(feature_class)
+        return _multi_linestring(features)
     elif shape_type in (GeometryType.polygon, GeometryType.multi_polygon):
-        return _build_multi_polygon(feature_class)
+        return _multi_polygon(features)
     else:
         raise ValueError(f'Unsupported shape type: {shape_type}')
 # End build_multi function
 
 
-def _get_validated_geoms(feature_class: 'FeatureClass',
-                         checker: Callable) -> list:
+def _multi_polygon(features: FeatureClass | ndarray) -> MultiPolygon:
     """
-    Get Shapely Geometries from Feature Class, forcing to 2D.
+    Build MultiPolygon from Polygon or MultiPolygon Feature Class or Geometries
     """
     geoms = []
-    cursor = feature_class.select()
-    while features := cursor.fetchmany(FETCH_SIZE):
-        geometries = to_shapely(features, option=DimensionOption.TWO_D)
-        for geometry in geometries:
-            for geom in get_geoms_iter(geometry):
-                if checker(geom) is None:
-                    continue
-                geoms.append(geom)
-    return geoms
-# End _get_validated_geoms function
-
-
-def _build_multi_polygon(feature_class: 'FeatureClass') -> MultiPolygon:
-    """
-    Build MultiPolygon from Polygon or MultiPolygon Feature Class
-    """
-    geoms = _get_validated_geoms(feature_class, checker=check_polygon)
+    checker = check_polygon
+    if isinstance(features, FeatureClass):
+        geoms = _get_validated_geoms(features, checker=checker)
+    else:
+        _check_geometries(features, checker=checker, geoms=geoms)
     if not geoms:
         return MultiPolygon()
     multi = unary_union(normalize(geoms)).normalize()
     prepare(multi)
     # noinspection PyTypeChecker
     return multi
-# End _build_multi_polygon function
+# End _multi_polygon function
 
 
-def _build_multi_linestring(feature_class: 'FeatureClass') -> MultiLineString:
+def _multi_linestring(features: FeatureClass | ndarray) -> MultiLineString:
     """
     Build MultiLineString from LineString or MultiLineString Feature Class
+    or Geometries
     """
-    geoms = _get_validated_geoms(feature_class, checker=check_linestring)
-    if not geoms:
-        return MultiLineString()
+    geoms = []
+    checker = check_linestring
+    if isinstance(features, FeatureClass):
+        geoms = _get_validated_geoms(features, checker=checker)
+    else:
+        _check_geometries(features, checker=checker, geoms=geoms)
     multi = MultiLineString(geoms)
     prepare(multi)
     return multi
-# End _build_multi_linestring function
+# End _multi_linestring function
 
 
-def _build_multi_point(feature_class: 'FeatureClass') -> MultiPoint:
+def _multi_point(features: FeatureClass | ndarray) -> MultiPoint:
     """
     Build MultiPoint from Point or MultiPoint Feature Class
     """
-    geoms = _get_validated_geoms(feature_class, checker=check_point)
-    if not geoms:
-        return MultiPoint()
+    geoms = []
+    checker = check_point
+    if isinstance(features, FeatureClass):
+        geoms = _get_validated_geoms(features, checker=checker)
+    else:
+        _check_geometries(features, checker=checker, geoms=geoms)
     multi = MultiPoint(geoms)
     prepare(multi)
     return multi
-# End _build_multi_point function
+# End _multi_point function
 
 
 if __name__ == '__main__':  # pragma: no cover
