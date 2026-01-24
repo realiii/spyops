9cef1a84b2ebee1a9f22e4957195b18765b8eba4
Jason Humber <jlhumber@openiii.com>
2026-01-24 05:49:46 -0600

    #144 Add `QueryMultiPartToSinglePart` class for multipart-to-singlepart queries in `management.features` module and corresponding tests

---
+++ spyops/query/management/features.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Query Classes for management.features module
+"""
+
+
+from spyops.query.base import AbstractSourceQuery
+from spyops.shared.constant import SQL_FULL
+from spyops.shared.field import (
+    ORIG_FID, get_geometry_column_name, make_field_names, make_unique_fields,
+    validate_fields)
+from spyops.shared.hint import FIELDS
+
+
+class QueryMultiPartToSinglePart(AbstractSourceQuery):
+    """
+    Queries for MultiPart to SinglePart
+    """
+    def _get_unique_fields(self) -> FIELDS:
+        """
+        Get Unique Fields and Rename Primary Key Columns if included
+        """
+        key = ORIG_FID.name.casefold()
+        fields = validate_fields(self.source, fields=self.source.fields)
+        names = [f.name.casefold() for f in fields]
+        if key not in names:
+            return ORIG_FID, *fields
+        index = names.index(key)
+        field, = make_unique_fields([ORIG_FID, *fields], others=[fields[index]])
+        fields[index] = field
+        return ORIG_FID, *fields
+    # End _get_unique_fields method
+
+    @property
+    def select(self) -> str:
+        """
+        Select from Source including FID
+        """
+        fields = validate_fields(self.source, fields=self.source.fields)
+        fields = [self.source.primary_key_field, *fields]
+        select_names = make_field_names(fields)
+        geom_type = get_geometry_column_name(
+            self.source, include_geom_type=True)
+        select_names = self._concatenate(geom_type, select_names)
+        return self._make_select(
+            self.source, field_names=select_names, where_clause=SQL_FULL)
+    # End select property
+
+    @property
+    def insert(self) -> str:
+        """
+        Insert Query
+        """
+        fields = self._get_unique_fields()
+        insert_names = make_field_names(fields)
+        geom = get_geometry_column_name(self.target)
+        insert_names = self._concatenate(geom, insert_names)
+        return self._make_insert(
+            self.target.escaped_name, field_names=insert_names,
+            field_count=len(fields) + 1)
+    # End insert property
+# End QueryMultiPartToSinglePart class
+
+
+if __name__ == '__main__':  # pragma: no cover
+    pass
