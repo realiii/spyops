e4e5f29a8c1e1eb21cf3377b89e94cc1172f3e7c
Jason Humber <jlhumber@openiii.com>
2026-01-24 17:59:30 -0600

    #47 Add: New constants for CRS support and area of interest validation

--- spyops/shared/constant.py
+++ spyops/shared/constant.py
@@ -69,6 +69,12 @@ NOT_IN: str = 'NOT IN'
 UNSUPPORTED_WKT: str = 'Unsupported WKT: {}'
 EMPTY_INPUT: str = '{}: {} is empty'
 UNABLE_TO_USE_CRS: str = 'Unable to use authority and code from {}'
+UNSUPPORTED_CRS: str = 'Unsupported CRS authority ({}) or code ({})'
+CRS_REQUIRED: str = 'A coordinate reference system is required'
+NO_TRANSFORMER: str = (
+    'No valid transformation exists for CRS {} ({}) and CRS {} ({})')
+INVALID_AOI: str = (
+    'The area of use "{}" does not intersect with the area of interest')
 
 
 BASE_TABLES: set[str] = {'gpkg_spatial_ref_sys', 'gpkg_contents',
