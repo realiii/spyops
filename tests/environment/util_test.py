8af5771f4d6e59c285b59c48ce0a7d30ec1e66e9
Jason Humber <jlhumber@openiii.com>
2026-02-02 10:41:44 -0600

    #47 Add: Implement `_scale_factor` and `get_grid_size` functions for CRS-based calculations, and add corresponding parameterized tests in `util_test.py`

--- tests/environment/util_test.py
+++ tests/environment/util_test.py
@@ -4,10 +4,13 @@ Test for Utility Functions
 """
 
 
-from pytest import mark
+from pyproj import CRS
+from pytest import approx, mark
 
+from spyops.crs.unit import get_unit_name
+from spyops.crs.util import get_crs_from_source, srs_from_crs
 from spyops.environment.enumeration import Setting
-from spyops.environment.util import as_title
+from spyops.environment.util import as_title, _scale_factor, get_grid_size
 
 
 pytestmark = [mark.environment]
@@ -29,6 +32,55 @@ def test_as_title(value, expected):
     """
     assert as_title(value) == expected
 # End test_as_title function
+
+
+@mark.parametrize('fc_name, expected', [
+    ('hydro_4617_a', 8.988709950585871e-06),
+    ('hydro_6654_a', 8.988710035851e-06),
+    ('hydro_lcc_a', 8.988709851109888e-06),
+    ('hydro_utm11_a', 8.988709851109888e-06),
+    ('toponymy_10tm_p', 8.988674167653699e-06),
+])
+def test_scale_factor(ntdb_zm_small, fc_name, expected):
+    """
+    Test _scale_factor
+    """
+    fc = ntdb_zm_small[fc_name]
+    assert approx(_scale_factor(fc), abs=10 ** -9) == expected
+# End test_scale_factor function
+
+
+@mark.parametrize('fc_name, epsg_code, from_name, to_name, expected', [
+    ('structures_10tm_a', 4326, 'metre', 'degree', 8.988679418564516e-05),
+    ('structures_4617_a', 4326, 'degree', 'degree', 10),
+    ('structures_6654_a', 4326, 'metre', 'degree', 8.988711179824804e-05),
+    ('structures_lcc_p', 4326, 'metre', 'degree', 8.98871027743553e-05),
+    ('structures_utm11_p', 4326, 'metre', 'degree', 8.98870804633134e-05),
+    ('structures_10tm_a', 6654, 'metre', 'metre', 10.),
+    ('structures_4617_a', 6654, 'degree', 'metre', 1112505.9844928253),
+    ('structures_6654_a', 6654, 'metre', 'metre', 10.),
+    ('structures_lcc_p', 6654, 'metre', 'metre', 10.),
+    ('structures_utm11_p', 6654, 'metre', 'metre', 10.),
+    ('structures_10tm_a', 2274, 'metre', 'US survey foot', 32.80833333333335),
+    ('structures_4617_a', 2274, 'degree', 'US survey foot', 3649946.71745688),
+    ('structures_6654_a', 2274, 'metre', 'US survey foot', 32.80833333333335),
+    ('structures_lcc_p', 2274, 'metre', 'US survey foot', 32.80833333333335),
+    ('structures_utm11_p', 2274, 'metre', 'US survey foot', 32.80833333333335),
+])
+def test_get_grid_size(ntdb_zm_small, fc_name, epsg_code, from_name, to_name, expected):
+    """
+    Test get grid size
+    """
+    xy_tolerance = 10.
+    fc = ntdb_zm_small[fc_name]
+    source_crs = get_crs_from_source(fc)
+    assert get_unit_name(source_crs) == from_name
+    target_crs = CRS(epsg_code)
+    assert get_unit_name(target_crs) == to_name
+    target_srs = srs_from_crs(target_crs)
+    size = get_grid_size(fc, xy_tolerance=xy_tolerance, target_srs=target_srs)
+    assert approx(size, abs=10 ** -9) == expected
+# End test_get_grid_size function
 
 
 if __name__ == '__main__':  # pragma: no cover
