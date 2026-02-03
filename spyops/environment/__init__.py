424bc56984616fba7eb71bd1ae2626b1f247532b
Jason Humber <jlhumber@openiii.com>
2026-02-03 09:15:14 -0600

    #48 Update: Add `Extent` to `__all__` in `environment/__init__.py` for module export consistency

--- spyops/environment/__init__.py
+++ spyops/environment/__init__.py
@@ -4,11 +4,18 @@ Package Initialization
 """
 
 
+from spyops.environment.base import Extent
 from spyops.environment.core import ANALYSIS_SETTINGS
 from spyops.environment.enumeration import OutputMOption, OutputZOption, Setting
 
 
-__all__ = ['ANALYSIS_SETTINGS', 'OutputMOption', 'OutputZOption', 'Setting']
+__all__ = [
+    'ANALYSIS_SETTINGS',
+    'OutputMOption',
+    'OutputZOption',
+    'Setting',
+    'Extent',
+]
 
 
 if __name__ == '__main__':  # pragma: no cover
