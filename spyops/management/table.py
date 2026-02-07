5c00914607eac0fe4a6b00b87a6d47d852e28494
Jason Humber <jlhumber@openiii.com>
2026-02-07 14:50:35 -0600

    #225 Add `create_table` function for geopackage table creation and its tests

--- spyops/management/table.py
+++ spyops/management/table.py
@@ -4,9 +4,15 @@ Data Management for Tables
 """
 
 
+from typing import TYPE_CHECKING
+
+from spyops.environment import ANALYSIS_SETTINGS
 from spyops.shared.constant import SOURCE
-from spyops.shared.hint import ELEMENT
-from spyops.validation import validate_element
+from spyops.shared.hint import ELEMENT, FIELDS, GPKG
+from spyops.validation import validate_element, validate_geopackage
+
+if TYPE_CHECKING:  # pragma: no cover
+    from fudgeo import Table
 
 
 @validate_element(SOURCE, has_content=False)
@@ -18,6 +24,20 @@ def get_count(source: ELEMENT) -> int:
     """
     return len(source)
 # End get_count function
+
+
+@validate_geopackage()
+def create_table(geopackage: GPKG, name: str, *, fields: FIELDS = (),
+                 description: str = '') -> 'Table':
+    """
+    Create Table
+
+    Create a new table in a geopackage with specified fields
+    """
+    overwrite = ANALYSIS_SETTINGS.overwrite
+    return geopackage.create_table(
+        name, fields=fields, description=description, overwrite=overwrite)
+# End create_table function
 
 
 if __name__ == '__main__':  # pragma: no cover
--- tests/management/table_test.py
+++ tests/management/table_test.py
@@ -4,10 +4,11 @@ Tests for Table Module
 """
 
 
+from fudgeo import Table
+from fudgeo.constant import FID
 from pytest import mark
 
-from spyops.management import get_count
-
+from spyops.management import get_count, create_table
 
 pytestmark = [mark.management, mark.table]
 
@@ -20,6 +21,17 @@ def test_get_count(ntdb_zm):
     result = get_count(source)
     assert result == 12_950
 # End test_get_count function
+
+
+def test_create_table(mem_gpkg):
+    """
+    Test create table
+    """
+    table_name = 'test_table'
+    result = create_table(mem_gpkg, table_name)
+    assert isinstance(result, Table)
+    assert [f.name for f in result.fields] == [FID]
+# End test_create_table function
 
 
 if __name__ == '__main__':  # pragma: no cover
