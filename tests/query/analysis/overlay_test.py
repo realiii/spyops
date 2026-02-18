37e2d725021c8bda27c264773f5494b37e1071ce
Jason Humber <jlhumber@openiii.com>
2026-02-18 14:46:02 -0600

    #71 Update `TestPlanarizeGeneral` to `TestPlanarizePoint`, refactor methods to use `PlanarizePointSource`

--- tests/query/analysis/overlay_test.py
+++ tests/query/analysis/overlay_test.py
@@ -16,10 +16,9 @@ from spyops.environment.core import zm_config
 from spyops.environment.enumeration import (
     OutputMOption, OutputZOption, Setting)
 from spyops.query.analysis.overlay import (
-    PlanarizeGeneralOperator, PlanarizeGeneralSource,
-    PlanarizeLineStringOperator, PlanarizeLineStringSource,
-    PlanarizePolygonOperator, PlanarizePolygonSource, QueryErase,
-    QueryIntersectClassic, QueryIntersectPairwise,
+    PlanarizePointSource, PlanarizeLineStringOperator,
+    PlanarizeLineStringSource, PlanarizePolygonOperator, PlanarizePolygonSource,
+    QueryErase, QueryIntersectClassic, QueryIntersectPairwise,
     QuerySymmetricalDifferenceClassic, QuerySymmetricalDifferencePairwise,
     QueryUnionClassic, QueryUnionPairwise)
 from spyops.shared.element import copy_element
@@ -341,71 +340,35 @@ class TestPlanarizeLineString:
 # End TestPlanarizeLineString class
 
 
-class TestPlanarizeGeneral:
+class TestPlanarizePoint:
     """
-    Test Planarize Line Strings and Points
+    Test Planarize Points
     """
-    @mark.parametrize('cls', [
-        PlanarizeGeneralSource,
-        PlanarizeGeneralOperator
-    ])
     @mark.parametrize('use_full_extent', [
         True, False
     ])
-    def test_planarize_extent(self, cls, inputs, world_features, mem_gpkg, use_full_extent):
+    def test_extent(self, inputs, world_features, mem_gpkg, use_full_extent):
         """
         Test Planarize Source Multi Part on a non FID column and using extent
         """
         operator = inputs['rivers_portion_l']
         source = world_features['admin_mp_a']
-        ps = cls(
+        ps = PlanarizePointSource(
             source=source, operator=operator,
             use_full_extent=use_full_extent, xy_tolerance=None)
         with Swap(Setting.EXTENT, Extent.from_bounds(7, 47, 16, 52, crs=CRS(4326))):
             assert ' 7.0 ' in ps.select
             assert ' 52.0 ' in ps.select
-    # End test_planarize_extent method
+    # End test_extent method
 
-    def test_planarize_source(self, inputs, world_features, mem_gpkg):
-        """
-        Test Planarize Source
-        """
-        operator = inputs['rivers_portion_l']
-        source = world_features['rivers_l']
-        ps = PlanarizeGeneralSource(source=source, operator=operator,
-                                    use_full_extent=False, xy_tolerance=None)
-        assert ps.temporary_fid_field.name == 'fid_rivers_l'
-        fc, fid_fld = ps()
-        assert fid_fld.name == 'fid'
-        assert fc.field_names == [
-            'fid', 'SHAPE', 'fid_rivers_l', 'FEATURE_ID', 'PART_ID',
-            'NAME', 'SYSTEM', 'MILES', 'KILOMETERS']
-        assert len(fc) == 135
-    # End test_planarize_source method
-
-    def test_planarize_operator(self, inputs, world_features, mem_gpkg):
-        """
-        Test Planarize Operator
-        """
-        operator = inputs['rivers_portion_l']
-        source = world_features['rivers_l']
-        po = PlanarizeGeneralOperator(source=source, operator=operator,
-                                      use_full_extent=False, xy_tolerance=None)
-        assert po.temporary_fid_field.name == 'fid_rivers_portion_l'
-        fc, fid_fld = po()
-        assert fid_fld.name == 'fid'
-        assert fc.field_names == ['fid', 'SHAPE', 'fid_rivers_portion_l', 'NAME', 'SYSTEM']
-        assert len(fc) == 134
-    # End test_planarize_operator method
-
-    def test_planarize_source_point(self, inputs, mem_gpkg):
+    def test_source_point(self, inputs, mem_gpkg):
         """
         Test Planarize Source Point
         """
         operator = inputs['rivers_portion_l']
         source = inputs['river_p']
-        ps = PlanarizeGeneralSource(source=source, operator=operator,
-                                    use_full_extent=False, xy_tolerance=None)
+        ps = PlanarizePointSource(source=source, operator=operator,
+                                  use_full_extent=False, xy_tolerance=None)
         assert ps.temporary_fid_field.name == 'fid_river_p'
         fc, fid_fld = ps()
         assert fid_fld.name == 'fid'
@@ -413,19 +376,19 @@ class TestPlanarizeGeneral:
             'fid', 'SHAPE', 'fid_river_p', 'NAME', 'SYSTEM', 'vertex_index',
             'vertex_part', 'vertex_part_index', 'distance', 'angle']
         assert len(fc) == 20620
-    # End test_planarize_source_point method
+    # End test_source_point method
 
-    def test_planarize_source_multi_part(self, inputs, world_features, mem_gpkg):
+    def test_source_multi_part(self, inputs, world_features, mem_gpkg):
         """
         Test Planarize Source Multi Part on a non FID column
         """
         operator = inputs['rivers_portion_l']
         source = world_features['admin_mp_a']
-        ps = PlanarizeGeneralSource(source=source, operator=operator,
-                                    use_full_extent=False, xy_tolerance=None)
+        ps = PlanarizePointSource(source=source, operator=operator,
+                                  use_full_extent=False, xy_tolerance=None)
         assert ps.temporary_fid_field.name == 'OBJECTID_admin_mp_a'
-    # End test_planarize_source_multi_part method
-# End TestPlanarizeGeneral class
+    # End test_source_multi_part method
+# End TestPlanarizePoint class
 
 
 class TestQueryIntersectClassic:
