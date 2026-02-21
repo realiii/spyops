b3577c50704ee09a335cefd1b33983f2d318fefd
Jason Humber <jlhumber@openiii.com>
2026-02-21 12:53:39 -0600

    #132 Add `GeometryAttribute` enumeration with various geometry-related constants

--- spyops/shared/enumeration.py
+++ spyops/shared/enumeration.py
@@ -55,5 +55,57 @@ class WeightOption(StrEnum):
 # End WeightOption class
 
 
+class GeometryAttribute(StrEnum):
+    """
+    GeometryAttributes
+    """
+    POINT_X = auto()
+    POINT_Y = auto()
+    POINT_Z = auto()
+    POINT_M = auto()
+
+    CENTROID_X = auto()
+    CENTROID_Y = auto()
+    CENTROID_Z = auto()
+    CENTROID_M = auto()
+
+    PART_COUNT = auto()
+    POINT_COUNT = auto()
+    HOLE_COUNT = auto()
+
+    EXTENT_MIN_X = auto()
+    EXTENT_MIN_Y = auto()
+    EXTENT_MIN_Z = auto()
+    EXTENT_MAX_X = auto()
+    EXTENT_MAX_Y = auto()
+    EXTENT_MAX_Z = auto()
+
+    LENGTH = auto()
+    LENGTH_GEODESIC = auto()
+
+    LINE_BEARING = auto()
+
+    LINE_START_X = auto()
+    LINE_START_Y = auto()
+    LINE_START_Z = auto()
+    LINE_START_M = auto()
+    LINE_END_X = auto()
+    LINE_END_Y = auto()
+    LINE_END_Z = auto()
+    LINE_END_M = auto()
+
+    INSIDE_X = auto()
+    INSIDE_Y = auto()
+    INSIDE_Z = auto()
+    INSIDE_M = auto()
+
+    AREA = auto()
+    AREA_GEODESIC = auto()
+
+    PERIMETER = auto()
+    PERIMETER_GEODESIC = auto()
+# End GeometryAttribute class
+
+
 if __name__ == '__main__':  # pragma: no cover
     pass
