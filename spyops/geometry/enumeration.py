5a55bbaa2eb2869979cc55f6926407c3b6a8a172
Jason Humber <jlhumber@openiii.com>
2026-02-24 06:20:54 -0600

    #133 Add `GeometryInvalidReason` enumeration for geometry validation

--- spyops/geometry/enumeration.py
+++ spyops/geometry/enumeration.py
@@ -17,5 +17,36 @@ class DimensionOption(StrEnum):
 # End DimensionOption class
 
 
+class GeometryInvalidReason(StrEnum):
+    """
+    Geometry Invalid Reason
+    """
+    BAD_EXTENT = auto()
+
+    EMPTY = auto()
+    NAN_Z = auto()
+    NAN_M = auto()
+
+    REPEATED_M = auto()
+    NON_MONOTONIC = auto()
+
+    EMPTY_POINT = auto()
+    EMPTY_PART = auto()
+
+    REPEATED_VERTICES = auto()
+
+    MISMATCH_Z = auto()
+    MISMATCH_M = auto()
+
+    SELF_INTERSECTION = auto()
+
+    UNCLOSED = auto()
+    UNCLOSED_RING = auto()
+    EMPTY_RING = auto()
+    EXTERIOR_ORIENTATION = auto()
+    INTERIOR_ORIENTATION = auto()
+# End GeometryInvalidReason class
+
+
 if __name__ == '__main__':  # pragma: no cover
     pass
