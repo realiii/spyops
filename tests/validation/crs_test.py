437e6800e7b170ebc29cd4db66f4c58bdf00244f
Jason Humber <jlhumber@openiii.com>
2026-03-04 12:53:15 -0600

    #153 Update tests to include `validate_coordinate_system` and rename `validate_crs` tests for clarity

--- tests/validation/crs_test.py
+++ tests/validation/crs_test.py
@@ -9,42 +9,74 @@ from fudgeo import SpatialReferenceSystem
 from pyproj import CRS
 
 from spyops.shared.exception import OperationsError
-from spyops.validation import validate_supported_crs
+from spyops.validation import validate_coordinate_system, validate_supported_crs
 
 
 pytestmark = [mark.validation]
 
 
-def test_validate_crs_same(mem_gpkg):
+def test_validate_supported_crs_same(mem_gpkg):
     """
-    Test validate crs -- same
+    Test validate supported crs -- same
     """
     @validate_supported_crs('a', 'b', same=True)
     def crs_function(a, b):
         pass
     srs_a = mem_gpkg.spatial_references[4326]
-    srs_b = SpatialReferenceSystem(name='NAD27', organization='EPSG', org_coord_sys_id=4267, definition=CRS(4267).to_wkt())
+    srs_b = SpatialReferenceSystem(
+        name='NAD27', organization='EPSG', org_coord_sys_id=4267,
+        definition=CRS(4267).to_wkt())
     fc_a = mem_gpkg.create_feature_class('aaa', srs=srs_a)
     fc_b = mem_gpkg.create_feature_class('bbb', srs=srs_b)
     with raises(OperationsError):
         crs_function(fc_a, fc_b)
-# End test_validate_crs_same function
+# End test_validate_supported_crs_same function
 
 
-def test_validate_crs_invalid(mem_gpkg):
+def test_validate_supported_crs_invalid(mem_gpkg):
     """
-    Test validate crs -- invalid Spatial Reference System
+    Test validate supported crs -- invalid Spatial Reference System
     """
     @validate_supported_crs('a', 'b')
     def crs_function(a, b):
         pass
     srs_a = mem_gpkg.spatial_references[4326]
-    srs_b = SpatialReferenceSystem(name='Unk', organization='ABCD', org_coord_sys_id=-1, definition='Undefined')
+    srs_b = SpatialReferenceSystem(
+        name='Unk', organization='ABCD', org_coord_sys_id=-1,
+        definition='Undefined')
     fc_a = mem_gpkg.create_feature_class('aaa', srs=srs_a)
     fc_b = mem_gpkg.create_feature_class('bbb', srs=srs_b)
     with raises(OperationsError):
         crs_function(fc_a, fc_b)
-# End test_validate_crs_invalid function
+# End test_validate_supported_crs_invalid function
+
+
+def test_validate_coordinate_system(mem_gpkg):
+    """
+    Test validate coordinate system -- same
+    """
+    @validate_coordinate_system('coord_sys')
+    def crs_function(coord_sys):
+        return coord_sys
+    srs = SpatialReferenceSystem(
+        name='NAD27', organization='EPSG', org_coord_sys_id=4267,
+        definition=CRS(4267).to_wkt())
+    assert crs_function(srs) is srs
+# End test_validate_coordinate_system function
+
+
+def test_validate_coordinate_system_invalid(mem_gpkg):
+    """
+    Test validate coordinate system -- invalid Spatial Reference System
+    """
+    @validate_coordinate_system('coord_sys')
+    def crs_function(coord_sys):
+        return coord_sys
+    srs = SpatialReferenceSystem(
+        name='Unk', organization='ABCD', org_coord_sys_id=-1, definition='Undefined')
+    with raises(OperationsError):
+        crs_function(srs)
+# End test_validate_coordinate_system_invalid function
 
 
 if __name__ == '__main__':  # pragma: no cover
