bd984dc061c72220db4dfe6c8831ee700813d4d0
Jason Humber <jlhumber@openiii.com>
2026-03-07 08:08:25 -0600

    #186 Add `stats` module with statistics functions and associated tests

---
+++ spyops/shared/stats.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Statistics Functions for use within SQLite
+"""
+
+
+from math import isfinite
+from statistics import (
+    StatisticsError, mode as _mode, stdev as _standard_deviation, variance)
+from typing import Any, Callable
+
+
+def _filter_none(values: list) -> list:
+    """
+    Filter None's out of a list
+    """
+    return [v for v in values if v is not None]
+# End _filter_none function
+
+
+def mode(values: list) -> Any:
+    """
+    Calculate Mode, ignoring Null values.  Works on a sequence of
+    hashable objects (strings, datetime, float, int, etc.).
+    """
+    if not (values := _filter_none(values)):
+        return None
+    try:
+        return _mode(values)
+    except (IndexError, TypeError):
+        return None
+# End mode function
+
+
+def stdev(values: list) -> float | None:
+    """
+    Calculate Standard Deviation, ignoring Null values and non-finite values.
+    If a non-number is encountered, the result is None.
+    """
+    return _calculate_stat(_standard_deviation, values)
+# End stdev function
+
+
+def var(values: list) -> float | None:
+    """
+    Calculate Variance, ignoring Null values and non-finite values.
+    If a non-number is encountered, the result is None.
+    """
+    return _calculate_stat(variance, values)
+# End var function
+
+
+def first(values: list) -> Any:
+    """
+    Find the first value, ignores Null values.
+    """
+    if not (values := _filter_none(values)):
+        return None
+    return values[0]
+# End first function
+
+
+def last(values: list) -> Any:
+    """
+    Find the last value, ignores Null values.
+    """
+    if not (values := _filter_none(values)):
+        return None
+    return values[-1]
+# End last function
+
+
+def _calculate_stat(func: Callable, values: list) -> float | None:
+    """
+    Calculate Statistic, ignoring Null values and non-finite values.
+    """
+    if not (values := _filter_none(values)):
+        return None
+    try:
+        if not (values := [v for v in values if isfinite(v)]):
+            return None
+    except TypeError:
+        return None
+    try:
+        return func(values)
+    except (IndexError, ValueError, StatisticsError):
+        return None
+# End _calculate_stat function
+
+
+STATS_FUNCS: dict[str, Callable] = {
+    'mode': mode,
+    'stdev': stdev,
+    'var': var,
+    'first': first,
+    'last': last,
+}
+
+
+if __name__ == '__main__':  # pragma: no cover
+    pass
---
+++ tests/shared/stats_test.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Test Statistics
+"""
+
+
+from pytest import mark, approx
+
+from spyops.shared.stats import first, last, mode, stdev, var
+
+pytestmark = [mark.statistics]
+
+
+@mark.parametrize('values, expected', [
+    ([], None),
+    ([[]], None),
+    ([1, 2, 3], 1),
+    ([1, 2, 2, 3], 2),
+    ([1, 2, 2, None, 3], 2),
+    (['a', 'b', 'c', None, 'c'], 'c'),
+])
+def test_mode(values, expected):
+    """
+    Test mode
+    """
+    assert mode(values) == expected
+# End test_mode function
+
+
+@mark.parametrize('values, expected', [
+    ([], None),
+    ([[]], None),
+    ([1, 2, 3], 1.),
+    ([1, 2, 3, 4, 5], 1.5811),
+    ([1, 2, None, 3], 1.),
+    (['a', 'b', 'c', None, 'c'], None),
+])
+def test_stdev(values, expected):
+    """
+    Test standard deviation
+    """
+    assert approx(stdev(values), abs=0.001) == expected
+# End test_stdev function
+
+
+@mark.parametrize('values, expected', [
+    ([], None),
+    ([[]], None),
+    ([1, 2, 3], 1.),
+    ([1, 2, 3, 4, 5], 2.5),
+    ([1, 2, None, 3], 1.),
+    (['a', 'b', 'c', None, 'c'], None),
+])
+def test_var(values, expected):
+    """
+    Test variance
+    """
+    assert approx(var(values), abs=0.001) == expected
+# End test_var function
+
+
+@mark.parametrize('values, expected', [
+    ([], None),
+    ([1, 2, 3], 1),
+    ([1, 2, 3, 4, 5], 1),
+    ([1, 2, None, 3], 1),
+    (['a', 'b', 'c', None, 'c'], 'a'),
+])
+def test_first(values, expected):
+    """
+    Test first value
+    """
+    assert first(values) == expected
+# End test_first function
+
+
+@mark.parametrize('values, expected', [
+    ([], None),
+    ([1, 2, 3], 3),
+    ([1, 2, 3, 4, 5], 5),
+    ([1, 2, None, 3], 3),
+    (['a', 'b', 'c', None, 'c'], 'c'),
+])
+def test_last(values, expected):
+    """
+    Test last value
+    """
+    assert last(values) == expected
+# End test_last function
+
+
+if __name__ == '__main__':  # pragma: no cover
+    pass
