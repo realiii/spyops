9139dd639a0e98856f4b29773eb3cf133b276687
Jason Humber <jlhumber@openiii.com>
2026-03-09 07:08:57 -0500

    #186 Refactor: Group statistical function tests into `TestSQLiteStatisticsFunctions` class

--- tests/shared/stats_test.py
+++ tests/shared/stats_test.py
@@ -11,98 +11,98 @@ from spyops.shared.stats import first, last, median, mode, stdev, var
 pytestmark = [mark.statistics]
 
 
-@mark.parametrize('values, expected', [
-    ([], None),
-    ([[]], None),
-    ([1, 2, 3], 1),
-    ([1, 2, 2, 3], 2),
-    ([1, 2, 2, None, 3], 2),
-    (['a', 'b', 'c', None, 'c'], 'c'),
-])
-def test_mode(values, expected):
+class TestSQLiteStatisticsFunctions:
     """
-    Test mode
+    Test SQLite Statistics Functions
     """
-    assert mode(values) == expected
-# End test_mode function
+    @mark.parametrize('values, expected', [
+        ([], None),
+        ([[]], None),
+        ([1, 2, 3], 1),
+        ([1, 2, 2, 3], 2),
+        ([1, 2, 2, None, 3], 2),
+        (['a', 'b', 'c', None, 'c'], 'c'),
+    ])
+    def test_mode(self, values, expected):
+        """
+        Test mode
+        """
+        assert mode(values) == expected
+    # End test_mode method
 
+    @mark.parametrize('values, expected', [
+        ([], None),
+        ([[]], None),
+        ([1, 2, 3], 1.),
+        ([1, 2, 3, 4, 5], 1.5811),
+        ([1, 2, None, 3], 1.),
+        (['a', 'b', 'c', None, 'c'], None),
+    ])
+    def test_stdev(self, values, expected):
+        """
+        Test standard deviation
+        """
+        assert approx(stdev(values), abs=0.001) == expected
+    # End test_stdev method
 
-@mark.parametrize('values, expected', [
-    ([], None),
-    ([[]], None),
-    ([1, 2, 3], 1.),
-    ([1, 2, 3, 4, 5], 1.5811),
-    ([1, 2, None, 3], 1.),
-    (['a', 'b', 'c', None, 'c'], None),
-])
-def test_stdev(values, expected):
-    """
-    Test standard deviation
-    """
-    assert approx(stdev(values), abs=0.001) == expected
-# End test_stdev function
+    @mark.parametrize('values, expected', [
+        ([], None),
+        ([[]], None),
+        ([1, 2, 3], 1.),
+        ([1, 2, 3, 4, 5], 2.5),
+        ([1, 2, None, 3], 1.),
+        (['a', 'b', 'c', None, 'c'], None),
+    ])
+    def test_var(self, values, expected):
+        """
+        Test variance
+        """
+        assert approx(var(values), abs=0.001) == expected
+    # End test_var method
 
+    @mark.parametrize('values, expected', [
+        ([], None),
+        ([[]], None),
+        ([1, 2, 3], 2),
+        ([1, 2, 3, 4, 5], 3),
+        ([1, 2, None, 3], 2),
+        (['a', 'b', 'c', None, 'c'], None),
+    ])
+    def test_median(self, values, expected):
+        """
+        Test median
+        """
+        assert approx(median(values), abs=0.001) == expected
+    # End test_median method
 
-@mark.parametrize('values, expected', [
-    ([], None),
-    ([[]], None),
-    ([1, 2, 3], 1.),
-    ([1, 2, 3, 4, 5], 2.5),
-    ([1, 2, None, 3], 1.),
-    (['a', 'b', 'c', None, 'c'], None),
-])
-def test_var(values, expected):
-    """
-    Test variance
-    """
-    assert approx(var(values), abs=0.001) == expected
-# End test_var function
+    @mark.parametrize('values, expected', [
+        ([], None),
+        ([1, 2, 3], 1),
+        ([1, 2, 3, 4, 5], 1),
+        ([1, 2, None, 3], 1),
+        (['a', 'b', 'c', None, 'c'], 'a'),
+    ])
+    def test_first(self, values, expected):
+        """
+        Test first value
+        """
+        assert first(values) == expected
+    # End test_first method
 
-
-@mark.parametrize('values, expected', [
-    ([], None),
-    ([[]], None),
-    ([1, 2, 3], 2),
-    ([1, 2, 3, 4, 5], 3),
-    ([1, 2, None, 3], 2),
-    (['a', 'b', 'c', None, 'c'], None),
-])
-def test_median(values, expected):
-    """
-    Test median
-    """
-    assert approx(median(values), abs=0.001) == expected
-# End test_median function
-
-
-@mark.parametrize('values, expected', [
-    ([], None),
-    ([1, 2, 3], 1),
-    ([1, 2, 3, 4, 5], 1),
-    ([1, 2, None, 3], 1),
-    (['a', 'b', 'c', None, 'c'], 'a'),
-])
-def test_first(values, expected):
-    """
-    Test first value
-    """
-    assert first(values) == expected
-# End test_first function
-
-
-@mark.parametrize('values, expected', [
-    ([], None),
-    ([1, 2, 3], 3),
-    ([1, 2, 3, 4, 5], 5),
-    ([1, 2, None, 3], 3),
-    (['a', 'b', 'c', None, 'c'], 'c'),
-])
-def test_last(values, expected):
-    """
-    Test last value
-    """
-    assert last(values) == expected
-# End test_last function
+    @mark.parametrize('values, expected', [
+        ([], None),
+        ([1, 2, 3], 3),
+        ([1, 2, 3, 4, 5], 5),
+        ([1, 2, None, 3], 3),
+        (['a', 'b', 'c', None, 'c'], 'c'),
+    ])
+    def test_last(self, values, expected):
+        """
+        Test last value
+        """
+        assert last(values) == expected
+    # End test_last method
+# End TestSQLiteStatisticsFunctions class
 
 
 if __name__ == '__main__':  # pragma: no cover
