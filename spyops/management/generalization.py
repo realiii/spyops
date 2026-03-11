171f2b667f8c3c143d31bb147e553ba5fb5692f5
Jason Humber <jlhumber@openiii.com>
2026-03-11 10:41:54 -0500

    #186 Add: `dissolve` function for feature aggregation and statistical summarization, and define `STATS_FIELDS` and `STATISTICS` aliases

---
+++ spyops/management/generalization.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Generalization Data Management
+"""
+
+
+from typing import TYPE_CHECKING
+
+from spyops.shared.hint import FIELDS, FIELD_NAMES, STATS_FIELDS, XY_TOL
+from spyops.shared.keywords import GROUP_FIELDS, SOURCE, STATISTICS
+from spyops.validation import (
+    validate_field, validate_overwrite_source, validate_result,
+    validate_source_feature_class, validate_statistic_field,
+    validate_target_feature_class, validate_xy_tolerance)
+
+
+if TYPE_CHECKING:  # pragma: no cover
+    from fudgeo import FeatureClass
+
+
+@validate_result()
+@validate_source_feature_class()
+@validate_target_feature_class()
+@validate_field(GROUP_FIELDS, element_name=SOURCE, exclude_primary=False)
+@validate_statistic_field(STATISTICS, element_name=SOURCE)
+@validate_xy_tolerance()
+@validate_overwrite_source()
+def dissolve(source: 'FeatureClass', target: 'FeatureClass',
+             group_fields: FIELDS | FIELD_NAMES, *,
+             statistics: STATS_FIELDS | None = None,
+             as_multi_part: bool = True, vertex_only: bool = False,
+             xy_tolerance: XY_TOL = None) -> 'FeatureClass':
+    """
+    Dissolve
+
+    Aggregate features based on one or more group fields and optionally
+    summarize attributes.  The as_multi_part option controls whether the
+    output is a single-part or multipart feature class.
+
+    The vertex_only option applies to lines only.  When True only those line
+    features that shared a common end-point will be dissolved into one another.
+    """
+    pass
+# End dissolve function
+
+
+if __name__ == '__main__':  # pragma: no cover
+    pass
