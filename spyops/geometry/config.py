6fcfff522038c3e3d56c8030feef8bbf9c0e3763
Jason Humber <jlhumber@openiii.com>
2026-03-13 12:01:34 -0500

    #186 revert to internal

--- spyops/geometry/config.py
+++ spyops/geometry/config.py
@@ -52,7 +52,7 @@ def geometry_config(target: 'FeatureClass', cast_geom: bool) -> GeometryConfig:
     shape_type = target.shape_type
     cls = FUDGEO_GEOMETRY_LOOKUP[shape_type][has_z, has_m]
     filter_types = SHAPELY_GEOMETRY_LOOKUP[shape_type]
-    combiner = get_combiner(shape_type, has_m=has_m)
+    combiner = _get_combiner(shape_type, has_m=has_m)
     srs_id = target.spatial_reference_system.srs_id
     if cast_geom:
         caster = GEOMETRY_CAST[target.shape_type]
@@ -123,7 +123,7 @@ def _make_measured_line(geom: LineString, has_z: bool, cls: Type[LineStringZM | 
 # End _make_measured_line function
 
 
-def get_combiner(shape_type: str, has_m: bool) -> Callable:
+def _get_combiner(shape_type: str, has_m: bool) -> Callable:
     """
     Get Combiner Function
     """
@@ -133,7 +133,7 @@ def get_combiner(shape_type: str, has_m: bool) -> Callable:
         else:
             return _combine_lines
     return nada
-# End get_combiner function
+# End _get_combiner function
 
 
 if __name__ == '__main__':  # pragma: no cover
