c8162a3efedd0e3e1eb37cd909a4c9883ecb6c88
Jason Humber <jlhumber@openiii.com>
2026-05-07 08:15:33 -0500

    #102 Refactor: Update `AbstractElementQuery` constructor defaults and extend `hint` imports with `SORT_FIELDS`

--- spyops/query/base.pyi
+++ spyops/query/base.pyi
@@ -18,7 +18,8 @@ from spyops.environment.core import HasZM, ZMConfig
 from spyops.geometry.config import GeometryConfig
 from spyops.shared.constant import EMPTY
 from spyops.shared.enumeration import AttributeOption
-from spyops.shared.hint import ELEMENT, EXTENT, FIELDS, GRID_SIZE, NAMES, XY_TOL
+from spyops.shared.hint import (
+    ELEMENT, EXTENT, FIELDS, GRID_SIZE, NAMES, SORT_FIELDS, XY_TOL)
 
 
 class AbstractElementQuery(metaclass=ABCMeta):
@@ -213,8 +214,8 @@ class BaseQuerySelect(AbstractSourceQuery):
 
     _where_clause: str
 
-    def __init__(self, source: FeatureClass, target: FeatureClass | Table,
-                 where_clause: str = ..., xy_tolerance: XY_TOL = ...) -> None: ...
+    def __init__(self, source: FeatureClass, target: ELEMENT,
+                 where_clause: str = EMPTY, xy_tolerance: XY_TOL = None) -> None: ...
     @property
     def select(self) -> str: ...
     @property
