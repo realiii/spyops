29c4c04a3f7bbe630b3f8b18c4d70c904dd40dd7
Jason Humber <jlhumber@openiii.com>
2026-05-11 08:07:40 -0500

    #34 Integrate `QueryFrequency` and `QueryStatistics` into type alias `QUERY_STAT`

--- spyops/analysis/util.py
+++ spyops/analysis/util.py
@@ -38,10 +38,12 @@ if TYPE_CHECKING:  # pragma: no cover
     from spyops.query.analysis.overlay import (
         QueryIntersectClassic, QueryIntersectPairwise,
         QuerySymmetricalDifferenceClassic, QuerySymmetricalDifferencePairwise)
+    from spyops.query.analysis.statistics import QueryFrequency, QueryStatistics
 
 
 QUERY_INT: TypeAlias = Union['QueryIntersectClassic', 'QueryIntersectPairwise']
 QUERY_SYM: TypeAlias = Union['QuerySymmetricalDifferenceClassic', 'QuerySymmetricalDifferencePairwise']
+QUERY_STAT: TypeAlias = Union['QueryStatistics', 'QueryFrequency']
 
 
 def _load_statistics(query: QUERY_STAT) -> Table:
