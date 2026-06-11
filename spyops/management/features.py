d09880f95e30cd0af2db1d64c66722bc25ca2b5e
Jason Humber <jlhumber@openiii.com>
2026-06-11 17:07:08 -0230

    #92 Refactor: update `xy_table_to_point` args to use `OPT_FIELD` type alias

--- spyops/management/features.py
+++ spyops/management/features.py
@@ -43,7 +43,8 @@ from spyops.shared.enumeration import (
     WeightOption)
 from spyops.shared.field import GEOM_TYPE_MULTI
 from spyops.shared.hint import (
-    ELEMENT, FEATURE_CLASSES, FIELDS, FIELD_NAMES, SORT_FIELDS, XY_TOL)
+    ELEMENT, FEATURE_CLASSES, FIELDS, FIELD_NAMES, OPT_FIELD, SORT_FIELDS,
+    XY_TOL)
 from spyops.shared.records import (
     extend_records, insert_many_features, select_and_transform_features)
 from spyops.validation import (
@@ -421,8 +422,8 @@ def repair_geometry(source: 'FeatureClass', drop_empty: bool = False) \
 def xy_table_to_point(source: ELEMENT, target: 'FeatureClass',
                       coordinate_system: CRS | SpatialReferenceSystem, *,
                       x_field: Field | str, y_field: Field | str,
-                      z_field: Field | str | None = None,
-                      m_field: Field | str | None = None) -> 'FeatureClass':
+                      z_field: OPT_FIELD = None,
+                      m_field: OPT_FIELD = None) -> 'FeatureClass':
     """
     XY Table to Point
 
