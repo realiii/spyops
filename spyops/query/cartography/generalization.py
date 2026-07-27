d7e36d66fbb2f93035f65371d72924a3076d5baf
Jason Humber <jlhumber@openiii.com>
2026-07-26 19:01:19 -0500

    #84 Feat: add `BaseQuerySmooth` and `QuerySmoothLine` classes for geometry smoothing

--- spyops/query/cartography/generalization.py
+++ spyops/query/cartography/generalization.py
@@ -6,9 +6,11 @@ Query Classes for cartography.generalization module
 
 from typing import Callable, TYPE_CHECKING
 
+from spyops.geometry.smooth import smooth_bezier, smooth_paek
 from spyops.geometry.wa import simplify
 from spyops.query.base import AbstractSourceQuery
-from spyops.shared.enumeration import SimplifyAlgorithmOption
+from spyops.shared.enumeration import (
+    SimplifyAlgorithmOption, SmoothAlgorithmOption)
 from spyops.shared.hint import XY_TOL
 
 
@@ -65,6 +67,53 @@ class QuerySimplifyPolygon(BaseQuerySimplify):
     Query Simplify Polygon
     """
 # End QuerySimplifyPolygon class
+
+
+class BaseQuerySmooth(AbstractSourceQuery):
+    """
+    Base Query Smooth
+    """
+    def __init__(self, source: 'FeatureClass', target: 'FeatureClass', *,
+                 algorithm_option: SmoothAlgorithmOption,
+                 where_clause: str, xy_tolerance: XY_TOL) -> None:
+        """
+        Initialize the BaseQuerySmooth class
+        """
+        super().__init__(source, target=target, where_clause=where_clause,
+                         xy_tolerance=xy_tolerance)
+        self._option: SmoothAlgorithmOption = algorithm_option
+    # End init built-in
+
+    @property
+    def smoother(self) -> Callable:
+        """
+        Smoother
+        """
+        if self._option == SmoothAlgorithmOption.BEZIER:
+            return smooth_bezier
+        else:
+            return smooth_paek
+    # End smoother property
+
+    @property
+    def insert(self) -> str:
+        """
+        Insert Query
+        """
+        elm = self.target
+        field_count, insert_field_names, _ = self._field_names_and_count(elm)
+        return self._make_insert(
+            elm.escaped_name, field_names=insert_field_names,
+            field_count=field_count)
+    # End insert property
+# End BaseQuerySmooth class
+
+
+class QuerySmoothLine(BaseQuerySmooth):
+    """
+    Query Smooth Line
+    """
+# End QuerySmoothLine class
 
 
 if __name__ == '__main__':  # pragma: no cover
