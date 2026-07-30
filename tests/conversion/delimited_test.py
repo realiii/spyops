e61f9ddcb6bb9cce3658bbe247412e3e723fdb0b
Jason Humber <jlhumber@openiii.com>
2026-07-30 08:29:03 -0500

    #352 Test: add `TestDelimitedFileToTable` class with parameterized test cases

--- tests/conversion/delimited_test.py
+++ tests/conversion/delimited_test.py
@@ -4,9 +4,10 @@ Tests for Delimited File Conversion
 """
 
 
+from fudgeo import Table
 from pytest import mark
 
-from spyops.conversion import table_to_delimited_file
+from spyops.conversion import delimited_file_to_table, table_to_delimited_file
 from spyops.shared.constant import COMMA, PIPE
 from spyops.shared.sort import Descending
 
@@ -47,6 +48,42 @@ class TestTableToDelimitedFile:
         assert output.stat().st_size > approx_size
     # End test_function method
 # End TestTableToDelimitedFile class
+
+
+class TestDelimitedFileToTable:
+    """
+    Test Delimited File to Table
+    """
+    @mark.parametrize('name, delimiter, count', [
+        ('badname.csv', COMMA, 7),
+        ('comma.csv', COMMA, 7),
+        ('empty_columns.csv', COMMA, 7),
+        ('good.csv', COMMA, 7),
+        ('header_data_mismatch.csv', COMMA, 139),
+        ('keywords.csv', COMMA, 7),
+        ('lower.csv', COMMA, 7),
+        ('missingdata.csv', COMMA, 7),
+        ('missingheader.csv', COMMA, 6),
+        ('missingname.csv', COMMA, 7),
+        ('nodata.csv', COMMA, 0),
+        ('null.csv', COMMA, 7),
+        ('null_as_string.csv', COMMA, 7),
+        ('pipe.csv', PIPE, 7),
+        ('poly.csv', COMMA, 7),
+        ('repeated_column.csv', COMMA, 7),
+    ])
+    def test_function(self, mem_gpkg, csv_path, name, delimiter, count):
+        """
+        Test delimited file to table
+        """
+        path = csv_path.joinpath(name)
+        assert path.is_file()
+        target = Table(mem_gpkg, name='csv_load')
+        tbl = delimited_file_to_table(
+            path, target=target, delimiter=delimiter)
+        assert len(tbl) == count
+    # End test_function method
+# End TestDelimitedFileToTable class
 
 
 if __name__ == '__main__':  # pragma: no cover
