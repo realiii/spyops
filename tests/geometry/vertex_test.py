8fd847cda708bf46c0571b9ad99c759e057642b2
Jason Humber <jlhumber@openiii.com>
2026-08-04 08:25:36 -0500

    #218 Test: update `test_mid_vertices_zm` to include Z and M average validation using `bottleneck.nanmean`

--- tests/geometry/vertex_test.py
+++ tests/geometry/vertex_test.py
@@ -4,11 +4,13 @@ Vertex Tests
 """
 
 
-from pytest import mark
+from bottleneck import nanmean
+from pytest import mark, approx
 
 from spyops.geometry.vertex import (
     GEOMETRY_VERTICES_ALL, GEOMETRY_VERTICES_BOTH_ENDS, GEOMETRY_VERTICES_END,
     GEOMETRY_VERTICES_MIDDLE, GEOMETRY_VERTICES_START)
+
 
 pytestmark = [mark.geometry]
 
@@ -120,33 +122,33 @@ def test_mid_vertices(world_features, fc_name, expected):
 
 
 @mark.zm
-@mark.parametrize('fc_name, expected', [
-    ('hydro_6654_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('hydro_6654_z_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('hydro_6654_m_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('hydro_6654_zm_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('structures_10tm_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}),
-    ('structures_10tm_z_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}),
-    ('structures_10tm_m_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}),
-    ('structures_10tm_zm_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}),
-    ('transmission_10tm_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('transmission_10tm_z_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('transmission_10tm_m_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('transmission_10tm_zm_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('transmission_10tm_ml', {1: 1, 2: 7, 3: 5, 4: 38}),
-    ('transmission_10tm_z_ml', {1: 1, 2: 7, 3: 5, 4: 38}),
-    ('transmission_10tm_m_ml', {1: 1, 2: 7, 3: 5, 4: 38}),
-    ('transmission_10tm_zm_ml', {1: 1, 2: 7, 3: 5, 4: 38}),
-    ('toponymy_6654_mp', {1: 1}),
-    ('toponymy_6654_z_mp', {1: 1}),
-    ('toponymy_6654_m_mp', {1: 1}),
-    ('toponymy_6654_zm_mp', {1: 1}),
-    ('transmission_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('transmission_z_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('transmission_m_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
-    ('transmission_zm_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}),
+@mark.parametrize('fc_name, expected, avg_z, avg_m', [
+    ('hydro_6654_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 0, 0),
+    ('hydro_6654_z_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 1230.3839, 0),
+    ('hydro_6654_m_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 0, 101007.0956),
+    ('hydro_6654_zm_a', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 1230.3839, 201007.0956),
+    ('structures_10tm_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}, 0, 101003.3288),
+    ('structures_10tm_z_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}, 1099.0560, 0),
+    ('structures_10tm_m_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}, 0, 101003.3288),
+    ('structures_10tm_zm_ma', {1: 7, 2: 1, 3: 156, 4: 1, 5: 8}, 1099.0560, 201003.3288),
+    ('transmission_10tm_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 0, 0),
+    ('transmission_10tm_z_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 1158.5081, 0),
+    ('transmission_10tm_m_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 0, 300002.4131),
+    ('transmission_10tm_zm_l', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 1158.5081, 400002.4131),
+    ('transmission_10tm_ml', {1: 1, 2: 7, 3: 5, 4: 38}, 0, 0),
+    ('transmission_10tm_z_ml', {1: 1, 2: 7, 3: 5, 4: 38}, 1166.1410, 0),
+    ('transmission_10tm_m_ml', {1: 1, 2: 7, 3: 5, 4: 38}, 0, 134441.9674),
+    ('transmission_10tm_zm_ml', {1: 1, 2: 7, 3: 5, 4: 38}, 1166.1410, 234441.9674),
+    ('toponymy_6654_mp', {1: 1}, 0, 0),
+    ('toponymy_6654_z_mp', {1: 1}, 1111.3894, 0),
+    ('toponymy_6654_m_mp', {1: 1}, 0, 500068.1239),
+    ('toponymy_6654_zm_mp', {1: 1}, 1111.3894, 600068.1239),
+    ('transmission_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 0, 0),
+    ('transmission_z_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 1162.7429, 0),
+    ('transmission_m_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 0, 500015.6),
+    ('transmission_zm_p', {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, 1162.7429, 600015.6),
 ])
-def test_mid_vertices_zm(ntdb_zm_small, fc_name, expected):
+def test_mid_vertices_zm(ntdb_zm_small, fc_name, expected, avg_z, avg_m):
     """
     Test mid vertices
     """
@@ -157,6 +159,11 @@ def test_mid_vertices_zm(ntdb_zm_small, fc_name, expected):
                   srs_id=source.spatial_reference_system.srs_id)
     counts = {k: len(v) for k, v in result.items()}
     assert counts == expected
+    points = sum(result.values(), [])
+    if source.has_z:
+        assert approx(nanmean([pt.z for pt in points]), abs=0.001) == avg_z
+    if source.has_m:
+        assert approx(nanmean([pt.m for pt in points]), abs=0.001) == avg_m
 # End test_mid_vertices_zm function
 
 
