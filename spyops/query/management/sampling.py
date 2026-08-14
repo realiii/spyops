e00b40ecefcf0a80b5a9c22e8446f1f357031bfc
Jason Humber <jlhumber@openiii.com>
2026-08-14 08:08:34 -0500

    #221 Refactor `_along_planar` to streamline logic with `_get_placement_details` integration and geometry construction updates.

--- spyops/query/management/sampling.py
+++ spyops/query/management/sampling.py
@@ -378,37 +378,25 @@ class AbstractQueryGeneratePointsAlongLines(AbstractQueryGenerateAlongLines,
         """
         return make_points(
             records, has_z=self.source.has_z, has_m=self.source.has_m)
-        return [(pt, attrs) for pt, (_, *attrs) in zip(points, records)]
-    # End _along_planar method
+    # End _build_geometries method
 
-    def _along_geodesic(self, features: list[tuple],
-                        geometries: 'ndarray', crs: 'CRS',
-                        getter: Callable) -> list[tuple['Point', tuple]]:
+    def _along_planar(self, features: list[tuple],
+                      geometries: 'ndarray', crs: 'CRS',
+                      getter: Callable) -> list[tuple['Point', tuple]]:
         """
-        Place Points Geodesic
+        Place Points Planar
         """
         records = []
-        details = get_equidistant_details(
-            geometries, crs=crs, has_z=self.source.has_z,
-            has_m=self.source.has_m)
-        for indexes, prj, to_eqd, from_eqd in details:
-            feats = [features[i] for i in indexes]
-            geoms = geometries[indexes]
-            if None in (to_eqd, from_eqd):
-                records.extend(self._along_planar(
-                    feats, geometries=geoms, crs=crs, getter=getter))
-                continue
-            else:
-                geoms = [getter(geom) for geom in geoms]
-                results = self._along_planar(
-                    feats, geometries=to_eqd(geoms), crs=prj, getter=nada)
-                if not results:  # pragma: no cover
-                    continue
-                points, attributes = zip(*results)
-                records.extend([(pt, attrs) for pt, attrs in
-                                zip(from_eqd(points), attributes)])
-        return records
-    # End _along_geodesic method
+        for details in self._get_placement_details(
+                features, geometries=geometries, crs=crs, getter=getter):
+            results = interpolate_locations(
+                details.distances, lengths=details.lengths,
+                coordinates=details.coordinates, ids=details.ids,
+                fid=details.fid, include_ends=self._config.include_ends)
+            records.extend(results)
+        geoms = self._build_geometries(records)
+        return [(pt, attrs) for pt, (_, *attrs) in zip(geoms, records)]
+    # End _along_planar method
 # End AbstractQueryGeneratePointsAlongLines class
 
 
