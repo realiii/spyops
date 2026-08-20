0ae0aaf5824e50ac5be00923f085a1abe30f8103
Jason Humber <jlhumber@openiii.com>
2026-08-20 14:01:23 -0500

    #221 Update docstrings in `generate_transects_along_lines` for clarity and consistent terminology

--- spyops/management/sampling.py
+++ spyops/management/sampling.py
@@ -120,14 +120,14 @@ def generate_transects_along_lines(
     or a string.  For example, Meters(100), DecimalDegrees(0.001), 100, or
     '100 meters'.  The length represents the total length of the transect.
 
-    Placement option DISTANCE creates points at the specified distance
+    Placement option DISTANCE creates transects at the specified distance
     interval, e.g. 100 metres will be placed at 200, 300, 400 meters, etc.
 
-    Placement option PERCENTAGE creates points at the specified percentage
+    Placement option PERCENTAGE creates transects at the specified percentage
     interval e.g. 15 percent will be placed at 15, 30, 45, 60, 75, and
     90 percent of the line length.
 
-    Placement option FIELD creates points differently based on the field
+    Placement option FIELD creates transects differently based on the field
     data type and field value.
 
     * Numeric fields are treated as being distances defined in the same units
@@ -137,14 +137,14 @@ def generate_transects_along_lines(
     * Text fields are treated as linear units and can contain one or more
       values separated by semicolons. If there is only one value in the text
       field, then it will be handled as an interval, for example, the string
-      value '50 feet' will be used to repeat points along the linear feature
+      value '50 feet' will be used to repeat transects along the linear feature
       at 50 ft intervals (e.g. 50, 100, ... 200, 250 ft, etc.)  If there are
       multiple values in the text field, then they will be treated as a list of
       specific distances, for example, the string value '50 feet;175 feet' will
-      be used to place two points, one at 50 ft and the other at 175 ft.
+      be used to place two transects, one at 50 ft and the other at 175 ft.
     * Units for text values can be mixed across features and within the same
       field. For example, the string value '50 feet;175 meters' will be used to
-      place two points, one at 50 ft and the other at 175 meters.  If a unit
+      place two transects, one at 50 ft and the other at 175 meters.  If a unit
       value is missing, then it will be assumed to be in the same units as the
       source feature class spatial reference.
 
