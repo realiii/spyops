0c7c9ccf05f143c046877f31692ff87da715e711
Jason Humber <jlhumber@openiii.com>
2026-09-20 12:55:52 -0500

    #310 Feature: add `generate_points_along_3d_lines` function, `threed` package, and comprehensive tests for 3D line point generation

---
+++ spyops/threed/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Package Initialization
+"""
+
+
+from spyops.shared.enumeration import DistanceTypeOption, PlacementOption
+from spyops.threed.features import generate_points_along_3d_lines
+
+__all__ = [
+    'generate_points_along_3d_lines',
+
+    'DistanceTypeOption',
+    'PlacementOption',
+]
+
+
+if __name__ == '__main__':  # pragma: no cover
+    pass
